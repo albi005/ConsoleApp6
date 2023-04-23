b3b302ece73dd1b9a707cc55358d282ce2790647
Albert Ragány-Németh <raganyalbi@gmail.com>
2023-04-23 13:23:23 +0200

    first commit

---
+++ ConsoleApp6/DasCrazy.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable once CheckNamespace
+namespace ConsoleApp6.Another;
+
+interface IA<T, AT> where AT : IA<T, AT>
+{
+    T Primary { get; set; }
+}
+
+class A<T, BT> : IA<T, BT> where BT : IA<T, BT>
+{
+    public T Primary { get; set; }
+}  
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Hello, World!");
+System.Interactive
+
+
+class A<T, AT> where AT : A<T, AT>
+{
+    public T Primary { get; set; }
+
+    public virtual NAT Convert<NT, NAT>(Func<T, NT> convert) where NAT : A<NT, NAT>, new()
+    {
+        return new NAT()
+        {
+            Primary = convert(Primary)
+        };
+    }
+}
+
+class B<T> : A<T, B<T>>
+{
+    public T Secondary { get; set; }
+
+    public override NAT Convert<NT, NAT>(Func<T, NT> convert) where NAT : B<NT>
+    {
+        NAT nat = base.Convert<NT, NAT>(convert);
+        return nat;
+    }
+}
